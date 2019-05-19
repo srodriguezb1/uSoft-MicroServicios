d65e9e1d337f1ed5ac7be4938f152870d0619f65
Ubuntu <ubuntu@ip-172-31-85-157.ec2.internal>
2019-05-19 23:43:22 +0000

    ventasDB added & running

--- src/ventas/ventas/settings.py
+++ src/ventas/ventas/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'ventas.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'vent',
+        'USER': 'vent',
+        'PASSWORD':'1234',
+        'HOST':'3.83.159.177',
+        'PORT':'5432',
     }
 }
 
