335a8e03a24760c1af81d49cce827b83762e000b
Ubuntu <ubuntu@ip-172-31-86-98.ec2.internal>
2019-05-19 23:47:44 +0000

    productosDB added and running

--- src/productos1/productos/settings.py
+++ src/productos1/productos/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'productos.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'produc',
+	'USER': 'produc',
+	'PASSWORD':'1234',
+	'HOST':'35.153.159.229',
+	'PORT':'5432',
     }
 }
 
