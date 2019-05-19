36e5db688f52f459f63196c63215ccfac30b46c2
Ubuntu <ubuntu@ip-172-31-86-223.ec2.internal>
2019-05-19 23:45:23 +0000

    cajasDB added and running

--- src/cajas/cajas/settings.py
+++ src/cajas/cajas/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'cajas.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'caja',
+        'USER': 'caja',
+        'PASSWORD':'1234',
+        'HOST':'54.227.215.37',
+        'PORT':'5432',
     }
 }
 
