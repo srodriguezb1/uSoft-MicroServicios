abc80778806a0b4cbd723d4ff6b28754710b77cc
Ubuntu <ubuntu@ip-172-31-83-40.ec2.internal>
2019-05-19 23:46:39 +0000

    facturasDB added and running

--- src/facturas/facturas/settings.py
+++ src/facturas/facturas/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'facturas.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'fact',
+        'USER': 'fact',
+        'PASSWORD':'1234',
+        'HOST':'54.165.160.218',
+        'PORT':'5432',
     }
 }
 
